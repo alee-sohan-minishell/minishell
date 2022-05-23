1134348c7d3c3441eb78836f257a77519ca5dcbf
sohan <sohan@student.42seoul.kr>
2022-05-23 18:21:01 +0900

    [feat] set signal handler SIGINT to print new line, Ignore SIGQUIT

--- signal/signal.c
+++ signal/signal.c
@@ -2,19 +2,23 @@
 #include <readline/readline.h>
 #include <signal.h>
 
-void	sigint_handler(int signo)
+static void	sigint_handler(int signo)
 {
-	if (signo == SIGINT)
-	{
 		rl_on_new_line();
-		//rl_replace_line("", 0);
-		//printf("\n");
+		rl_replace_line("", 0);
+		printf("\n");
 		rl_redisplay();
-	}
+		(void)signo;
 }
 
 void	set_signal_handler()
 {
 	signal(SIGINT, sigint_handler);
 	signal(SIGQUIT, SIG_IGN);
+}
+
+void	set_signal_default()
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
 }
---
+++ signal/signal.h
@@ -0,0 +1,7 @@
+#ifndef SIGNAL_H
+# define SIGNAL_H
+
+void	set_signal_handler(void);
+void	set_signal_default(void);
+
+#endif
