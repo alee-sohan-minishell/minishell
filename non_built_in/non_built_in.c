f048f2d4a8369084f21913b89910fffa06a8f265
codewhite7777 <codewhite7777@gmail.com>
2022-05-25 14:45:07 +0900

    [test code] non_built

--- non_built_in/non_built_in.c
+++ non_built_in/non_built_in.c
@@ -5,35 +5,50 @@
 #include <errno.h>
 #include <string.h>
 
+//debug
+#include "../env/env_list_interface_01.h"
+#include "../libft/libft.h"
+
 int	ft_exec_command(t_shell_data *p_data)
 {
 	pid_t	pid;
-	char	*path;
+	char	path[500];
 	char	*exec_path;
 	int		index;
 
 	exec_path = NULL;
+	path[0] = '\0';
 	index = 0;
 	pid = fork();
 	set_signal_foreground();
+
+	//debug
+	t_env_node	*cur_node;
+
+	env_node_search(&p_data->env_list, "PATH", &cur_node);
+	char **cmd_list = ft_split(cur_node->value, ':');
+	int	idx;
+
+	idx = 0;
 	if (pid == 0)
 	{
 		set_tc_attr_to_default(p_data);
-		path = "/bin/";
-		exec_path = ft_strjoin(path, p_data->cmd[0]);
-		if (execve(exec_path, p_data->cmd, NULL) == -1)
+		while (cmd_list[idx])
 		{
-			printf("bash: %s: %s\n", p_data->cmd[0], strerror(errno));
-			exit(errno);
+			ft_strlcpy(path, cmd_list[idx], 500);
+			ft_strlcat(path, "/", 500);
+			exec_path = ft_strjoin(path, p_data->cmd[0]);
+			printf("command : [%s] \n", exec_path);
+			if (execve(exec_path, p_data->cmd, NULL) == -1)
+			{
+				printf("bash: %s: %s\n", p_data->cmd[0], strerror(errno));
+				exit(errno);
+			}
+			idx++;
 		}
+		printf("no exist command \n");
 	}
 	wait(&pid);
-	while (p_data->cmd[index])
-	{
-		free(p_data->cmd[index]);
-		++index;
-	}
-	free(p_data->cmd);
 	free(exec_path);
 	return (0);
 }
