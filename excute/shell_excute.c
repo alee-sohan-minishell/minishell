1ba322edbdab306227c12f3032dd9d2d12bdfaa8
sohan <sohan@student.42seoul.kr>
2022-05-29 21:57:54 +0900

    [fix] restore STDIN STDOUT fd before and after execution, remove free part to prevent double free

--- excute/shell_excute.c
+++ excute/shell_excute.c
@@ -30,6 +30,7 @@ void	shell_excute(t_shell_data *p_data)
 {
 	if (!p_data)
 		return ;
+	//if (!p_data->cmd)
 	if (!p_data->cmd[0])
 		ft_set_status(p_data, S_LINE_READ);
 	else if (ft_strcmp(p_data->cmd[0], "cd") == 0)
@@ -58,27 +59,33 @@ void	shell_excute(t_shell_data *p_data)
 	}
 	else
 	{
+		dup2(p_data->fd_out_old, STDOUT_FILENO);
+		close(p_data->fd_out_old);
 		ft_exec_command(p_data);
 		set_tc_attr(p_data);
-		printf("%d\n", p_data->term_status);
+		dup2(p_data->fd_in_old, STDIN_FILENO);
+		close(p_data->fd_in_old);
+		//printf("%d\n", p_data->term_status);
 	}
 
 
 	/*	split free	*/
-	int	idx;
+	/*int	idx;
 
 	idx = 0;
-	while (p_data->cmd[idx])
+	while (p_data->cmd && p_data->cmd[idx])
 	{
 		free(p_data->cmd[idx]);
 		idx++;
 	}
-	free(p_data->cmd);
+	//free(p_data->cmd[idx]);
+	if (p_data->cmd)
+		free(p_data->cmd);*/
 	/*	-------------------------	*/
 
 	/*	line string free	*/
-	if (p_data->line)
-		free(p_data->line);
+	//if (p_data->line)
+	//	free(p_data->line);
 	/*	-------------------------	*/
 	/*	default home path free -> 프로그램 종료	*/
 	/*	env list free -> 프로그램 종료	*/
