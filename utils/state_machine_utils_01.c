c29ea201668ef328801eb87f2c32f46054ff1e8e
sohan <sohan@student.42seoul.kr>
2022-06-29 13:57:54 +0900

    [feat] set termattr to default when error exit

--- utils/state_machine_utils_01.c
+++ utils/state_machine_utils_01.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include "../env/env_list_interface_01.h"
 #include "../parse/shell_parse.h"
+#include "../init/shell_utils_01.h"
 
 void	ft_set_status(t_shell_data *p_data, t_shell_status status)
 {
@@ -29,5 +30,6 @@ int	status_close(t_shell_data *p_data)
 	if (p_data->env_default_home)
 		free(p_data->env_default_home);
 	env_node_clear(&p_data->env_list);
+	set_tc_attr_to_default(p_data);
 	return (1);
 }
